9589f9beae33736aae555307d475b8510492b43b
melashu <meshu102@outlook.com>
2022-07-18 17:36:35 +0300

    Update dynamic style for menulist

--- vbscripts/navigationScript.js
+++ vbscripts/navigationScript.js
@@ -1,11 +1,27 @@
+let menuIcon = document.getElementById("hambuger-menu");
+let navBar = document.getElementById("nav-bar");
+let menuList = document.querySelector(".nav-link");
+let main = document.querySelector("main");
+let header = document.querySelector("header");
+let email = document.getElementById("nav-mail");
 
-let menuIcon = document.getElementById('hambuger-menu');
-let navBar = document.getElementById('nav-bar');
-let menuList = document.querySelector('.nav-link');
+menuIcon.addEventListener("click", () => {
+  if (menuList.style.display === "none") {
+    email.style.display = 'none';
+    menuList.style.display = "flex";
+    menuList.style.flexDirection = "column";
+    menuList.style.height = "100vh";
+    menuList.style.color = "#fff";
+    menuList.style.fontSize = "32px";
+    menuList.style.order = -2;
+    menuList.style.marginTop = "20px";
 
-menuIcon.addEventListener('click', () => {
- 
-   menuList.style.display ='block';
-
-   
+    menuIcon.style.opacity = 0;
+    header.style.position = "absolute";
+    header.style.height = "100vh";
+    header.style.zIndex = "1";
+    //  main.style.display = "none";
+  } else {
+    menuList.style.display = "none";
+  }
 });
