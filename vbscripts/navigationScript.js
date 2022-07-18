520b90fcd11efbdb14d09f5952bed65f3e3d0a40
Rikstone <tarikbouari@gmail.com>
2022-07-18 13:52:05 +0000

    update nav-bar menu structure

--- vbscripts/navigationScript.js
+++ vbscripts/navigationScript.js
@@ -1,11 +1,11 @@
 
 let menuIcon = document.getElementById('hambuger-menu');
 let navBar = document.getElementById('nav-bar');
-let menuList = document.querySelector('.em');
+let menuList = document.querySelector('.nav-link');
 
 menuIcon.addEventListener('click', () => {
  
-   menuList.style.display = 'block';
-   console.log('hello');
+   menuList.style.display ='block';
+
    
 });
