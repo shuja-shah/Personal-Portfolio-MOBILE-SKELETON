7bfb3a17e71bfa281dccb59a9e203d5b3d5fb2b7
Rikstone <tarikbouari@gmail.com>
2022-07-18 13:46:33 +0000

    target menuList element

---
+++ vbscripts/navigationScript.js
@@ -0,0 +1,11 @@
+
+let menuIcon = document.getElementById('hambuger-menu');
+let navBar = document.getElementById('nav-bar');
+let menuList = document.querySelector('.em');
+
+menuIcon.addEventListener('click', () => {
+ 
+   menuList.style.display = 'block';
+   console.log('hello');
+   
+});
