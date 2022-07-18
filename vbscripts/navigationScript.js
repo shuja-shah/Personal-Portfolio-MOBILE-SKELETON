103e2d73c8ee90528c6dedcb1d68d3d45877bb7d
melashu <meshu102@outlook.com>
2022-07-18 17:46:24 +0300

    Add dynamic style for header

--- vbscripts/navigationScript.js
+++ vbscripts/navigationScript.js
@@ -7,10 +7,12 @@ let email = document.getElementById("nav-mail");
 
 menuIcon.addEventListener("click", () => {
   if (menuList.style.display === "none") {
-    email.style.display = 'none';
+    email.style.display = "none";
     menuList.style.display = "flex";
     menuList.style.flexDirection = "column";
     menuList.style.height = "100vh";
+    menuList.style.width = "95vw";
+
     menuList.style.color = "#fff";
     menuList.style.fontSize = "32px";
     menuList.style.order = -2;
@@ -20,7 +22,6 @@ menuIcon.addEventListener("click", () => {
     header.style.position = "absolute";
     header.style.height = "100vh";
     header.style.zIndex = "1";
-    //  main.style.display = "none";
   } else {
     menuList.style.display = "none";
   }
