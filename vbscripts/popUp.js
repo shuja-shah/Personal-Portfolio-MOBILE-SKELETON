845d698962a4aba4c2fde17127a4947fd7ae2e81
melashu <meshu102@outlook.com>
2022-07-20 12:27:59 +0300

    Update popup window html structure

--- vbscripts/popUp.js
+++ vbscripts/popUp.js
@@ -1,112 +1,124 @@
 const project = [
   {
-    projectTitle: 'Profesional Art Printing Data',
+    projectTitle: "Profesional Art Printing Data",
     projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
     techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'Ruby',
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "Ruby",
     },
-    projectImage: 'assets/pro3.png',
+    projectImage: "assets/pro3.png",
   },
   {
-    projectTitle: 'Data Dashboard Healthcare',
+    projectTitle: "Data Dashboard Healthcare",
     projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
     techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'Ruby',
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "Ruby",
     },
-    projectImage: 'assets/pro4.png',
+    projectImage: "assets/pro4.png",
   },
   {
-    projectTitle: 'Profesional Art Printing Data',
+    projectTitle: "Profesional Art Printing Data",
     projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
     techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'ruby',
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "ruby",
     },
-    projectImage: 'assets/pro5.png',
+    projectImage: "assets/pro5.png",
   },
   {
-    projectTitle: 'Profesional Art Printing Data',
+    projectTitle: "Profesional Art Printing Data",
     projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
     techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'ruby',
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "ruby",
     },
-    projectImage: 'assets/pro3.png',
-  },
-
-  {
-    projectTitle: 'Profesional Art Printing Data',
-    projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
-    techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'ruby',
-    },
-    projectImage: 'assets/pro4.png',
+    projectImage: "assets/pro3.png",
   },
 
   {
-    projectTitle: 'Profesional Art Printing Data',
+    projectTitle: "Profesional Art Printing Data",
     projectDiscription:
-      ' A daily selection of privately personalized reads; no accounts or sign-ups required. has'
-      + 'been the industrys standard',
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
     techTools: {
-      html: 'HTML',
-      bootstrap: 'Bootstrap',
-      ruby: 'ruby',
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "ruby",
     },
-    projectImage: 'assets/pro5.png',
+    projectImage: "assets/pro4.png",
+  },
+
+  {
+    projectTitle: "Profesional Art Printing Data",
+    projectDiscription:
+      " A daily selection of privately personalized reads; no accounts or sign-ups required. has" +
+      "been the industrys standard",
+    techTools: {
+      html: "HTML",
+      bootstrap: "Bootstrap",
+      ruby: "ruby",
+    },
+    projectImage: "assets/pro5.png",
   },
 ];
 
-const button = document.querySelectorAll('.btn-primary');
-const load = document.querySelector('Load');
+const button = document.querySelectorAll(".btn-primary");
+const load = document.querySelector("Load");
 
-const projectDiv = document.createElement('div');
-projectDiv.className = 'project-div';
-const divtemplate = document.getElementById('template'); 
-const closeBtn = document.getElementById('popup-close-btn');
+const projectDiv = document.createElement("div");
+projectDiv.className = "project-div";
+const divtemplate = document.getElementById("template");
+const closeBtn = document.getElementById("popup-close-btn");
 
 function initialize(project) {
- 
+  window.scrollTo(0, 0);
+  divtemplate.style.display = "block";
   let template = `
     <div class="project-text-div">
+   <div class='title-container'>
     <h3>${project.projectTitle}</h3>
+    <span class="popup-close-btn" id="popup-close-btn">&times;</span>
+   </div>
+
     <ul>
-    <li>${project.projectDiscription}</li>
     <li>${project.techTools.html}</li>
     <li>${project.techTools.bootstrap}</li>
     <li>${project.techTools.ruby}</li>
     </ul> 
-    </div>  
+  
     <div class="project-image-div">
     <img src="${project.projectImage}" alt="${project.projectTitle}">
-    </div>`
-        
-  ;
+    
+    </div>
+          <div class="popup-description-container">
+                    <p class="description" id="description">${project.projectDiscription}</p>
+                    <div class="popup-button">
+                        <a href="#" class="popup-btn submit-button" id="btn-live">See Live</a>
+                        <a href="#" class="popup-btn submit-button" id="btn-source">See Sources</a>
+                    </div>
+                </div>
+
+                   </div> 
+    `;
+
   divtemplate.innerHTML = template;
-  console.log(template);git 
-  console.log(divtemplate);
 }
 
 button.forEach((btn) => {
-  btn.addEventListener('click', (identity) => {
+  btn.addEventListener("click", (identity) => {
     const btnTity = identity.target.dataset.id;
     initialize(project[btnTity]);
     // console.log(project[btnTity]);
