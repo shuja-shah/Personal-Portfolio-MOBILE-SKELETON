b40e6f6a768f493041fc4b2f62af6b56a27fc865
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-07-21 13:42:00 -0700

    FInalize Error Message to Render from JavaScript

--- vbscripts/form.js
+++ vbscripts/form.js
@@ -1,18 +1,15 @@
-
 const form = document.getElementsByClassName('contact-form')[0];
 const ourEmail = document.getElementsByClassName('at')[0];
 const errorMessage = document.getElementsByClassName('error-message')[0];
 
-
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (ourEmail.value === ourEmail.value.toLowerCase()) {
     form.submit();
   } else {
     ourEmail.focus();
-    errorMessage.innerHTML =
-      "The form in not sent, Please enter lowercase email look likes above!";
+    errorMessage.innerHTML = 'Email Can not contain Uppercase Letter!';
     ourEmail.value = ourEmail.value.toLowerCase();
+    errorMessage.style.height = '10px';
   }
 });
-
