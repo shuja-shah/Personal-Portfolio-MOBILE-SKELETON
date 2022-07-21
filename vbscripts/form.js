0c567f149389b9c2a749cdab8690c4ab60186ef8
melashu <meshu102@outlook.com>
2022-07-21 13:17:00 +0300

    chnage email variable

--- vbscripts/form.js
+++ vbscripts/form.js
@@ -1,17 +1,18 @@
 
 const form = document.getElementsByClassName('contact-form')[0];
-const email = document.getElementsByClassName('at')[0];
+const ourEmail = document.getElementsByClassName('at')[0];
 const errorMessage = document.getElementsByClassName('error-message')[0];
 
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (ourEmail.value === ourEmail.value.toLowerCase()) {
+    form.submit();
+  } else {
+    ourEmail.focus();
+    errorMessage.innerHTML =
+      "The form in not sent, Please enter lowercase email look likes above!";
+    ourEmail.value = ourEmail.value.toLowerCase();
+  }
+});
 
-    if (email.value == email.toLowerCase()) {
-        e.preventDefault();
-        form.submit();
-    } else {
-        errorMessage.innerHTML = 'The for is not sent! Please make the email lowercase!';
-        email.focus();
-    }
-})
