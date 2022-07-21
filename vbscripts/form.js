7b8859e35f5d0ef67c690470cbd911c28c028236
melashu <meshu102@outlook.com>
2022-07-21 13:06:08 +0300

    Add validation rule

--- vbscripts/form.js
+++ vbscripts/form.js
@@ -1,13 +1,17 @@
 
 const form = document.getElementsByClassName('contact-form')[0];
 const email = document.getElementsByClassName('at')[0];
-const errorMessage = document.getElementsByClassName('error-message');
+const errorMessage = document.getElementsByClassName('error-message')[0];
 
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
 
-    if(email.value == email.toLower.case()){
-        
+    if (email.value == email.toLowerCase()) {
+        e.preventDefault();
+        form.submit();
+    } else {
+        errorMessage.innerHTML = 'The for is not sent! Please make the email lowercase!';
+        email.focus();
     }
 })
