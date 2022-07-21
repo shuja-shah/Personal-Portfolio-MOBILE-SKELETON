28a0efae996206ed30f73e144c65d47788ce6e34
Rikstone <tarikbouari@gmail.com>
2022-07-21 09:53:25 +0000

    declare variable in form.js

---
+++ vbscripts/form.js
@@ -0,0 +1,9 @@
+
+const form = document.getElementsByClassName('contact-form')[0];
+const email = document.getElementsByClassName('at')[0];
+const errorMessage = document.getElementsByClassName('error-message');
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+})
